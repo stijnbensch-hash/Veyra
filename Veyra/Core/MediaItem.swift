ce8ef036382cb5b568a4704ee3fb060a6c00bcde
Stijn Bensch <stijn.bensch@me.com>
2026-09-16 12:20:02 +0200

    Add IMDb identifier to media model

--- Veyra/Core/MediaItem.swift
+++ Veyra/Core/MediaItem.swift
@@ -4,15 +4,18 @@ struct MediaItem: Identifiable, Hashable {
     let id: UUID
     let title: String
     let type: MediaType
+    let imdbID: String?
 
     init(
         id: UUID = UUID(),
         title: String,
-        type: MediaType
+        type: MediaType,
+        imdbID: String? = nil
     ) {
         self.id = id
         self.title = title
         self.type = type
+        self.imdbID = imdbID
     }
 }
 
