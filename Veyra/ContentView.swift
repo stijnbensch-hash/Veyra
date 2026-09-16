2b05570c0bbbd40c1756e61c3ff455da4132578b
Stijn Bensch <stijn.bensch@me.com>
2026-09-16 07:03:41 +0200

    Initial Commit

---
+++ Veyra/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Veyra
+//
+//  Created by Stijn Bensch on 16/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Veyra/VeyraApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeyraApp.swift
+//  Veyra
+//
+//  Created by Stijn Bensch on 16/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct VeyraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
